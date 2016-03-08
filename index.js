a475bb81f6d1594426d99024934078c2660a53f9
Sal <salvatore.niro@outlook.com>
2016-03-08 13:46:26 -0700

    wild west: let's get something working!

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// like woah
+var fs = require('fs');
+var minify = require('html-minifier').minify;
+var engine = new TemplateEngine();
+
+
+function TemplateEngine() {
+    var self = this;
+
+    self.getDirectiveTemplateHash = function(raw){
+
+        var arr = []
+        var found = raw.match(/(?!,)templateUrl(.*)$/gm)[0];
+
+        if(found) {
+            arr.push(found.split(':')[1].split(',')[0].replace(/"/g, '').replace(/'/g, '').trim());
+        }
+
+        return arr;
+    }
+
+    self.read = function(target, done) {
+        fs.readFile(__dirname + target, 'utf8', function (err, data) {
+            done(data);
+        });
+    }
+}
+
+function TemplateManager() {
+
+    var self = this;
+
+    self.inline = function (target, options, done) {
+
+        self.read(target, function(data){
+
+            engine.getDirectiveTemplateHash(data)
+            done();
+        });
+    }
+}
+
+module.exports = new TemplateManager();
+module.exports.engine = engine;
--- test/test.js
+++ test/test.js
@@ -1,11 +1,70 @@
 var chai = require('chai');
 var expect = chai.expect;
+var tpl2js = require('..');
+var engine = require('..').engine;
 
 chai.should();
 
-describe('init', function () {
+describe('tpl2js: init', function () {
 
     it('should init', function () {
-        expect(1).to.equal(1);
+        expect(tpl2js.inline).to.be.function;
     });
 });
+
+describe('tpl2js: engine', function () {
+
+    it('should read the JS file: allow through', function (done) {
+        var i = 0;
+
+        engine.read('/test/fixtures/js/ng.module.basic.js', function () {
+            i += 1;
+            expect(i).to.equal(1);
+            done();
+        });
+    });
+
+    it('should read the JS file: verify results', function (done) {
+
+        var expected = `angular.module('mod').directive('dir', function () {
+                                return {
+                                    scope: {},
+                                    templateUrl: 'templates/ng.template.basic.html',
+                                    link: function (scope, elem, attrs) {
+                                    }
+                                }
+                            });`
+
+        engine.read('/test/fixtures/js/ng.module.basic.js', function (data) {
+            expect(data.replace(/ /g, '').replace(/(\r\n|\n|\r)/gm, '')).to.equal(expected.replace(/ /g, '').replace(/(\r\n|\n|\r)/gm, ''));
+            done();
+        });
+    });
+
+    it('should resolve the correct directive template hash: single file', function () {
+
+        var raw = `angular.module('mod').directive('dir', function () {
+                    return {
+                        scope: {},
+                        templateUrl: 'templates/ng.template.basic.html',
+                        link: function (scope, elem, attrs) {
+                        }
+                    }
+                });`
+
+        var expected = ['templates/ng.template.basic.html'];
+
+        var hash = engine.getDirectiveTemplateHash(raw)
+        expect(hash).to.deep.equal(expected);
+    });
+
+    it('should resolve the correct directive template hash: single file: minified', function () {
+
+        var raw = 'angular.module("mod").directive("dir",function(){return{scope:{},templateUrl:"templates/ng.template.basic.html",link:function(e,t,l){}}});';
+        var expected = ['templates/ng.template.basic.html'];
+
+        var hash = engine.getDirectiveTemplateHash(raw)
+        expect(hash).to.deep.equal(expected);
+    });
+});
+
